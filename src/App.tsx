850b8ba2061e2352a92f650effa708b300dad819
Alex41099 <nevazhnoalikhan@gmail.com>
2024-12-17 16:03:04 +0500

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styles from "./components/Site.module.css";
+import { Adidas } from "./components/pages/Adidas";
+import { Puma } from "./components/pages/Puma";
+import { Abibas } from "./components/pages/Abibas";
+import { Navigate, NavLink, Outlet, Route, Routes } from "react-router-dom";
+import { Error404 } from "./components/pages/Error404";
+import styled from "styled-components";
+import { S } from "./components/pages/_styles";
+import { Model } from "./components/pages/Model";
+import { Prices } from "./components/pages/Prices";
+import { PATH } from "./app/routes/router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={styles.header}>
+        <h1>HEADER</h1>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.nav}>
+          <S.NavWrapper>
+            <NavLink to={PATH.ADIDAS}>Adidas</NavLink>
+          </S.NavWrapper>
+          <S.NavWrapper>
+            <NavLink to={PATH.PUMA}>Puma</NavLink>
+          </S.NavWrapper>
+          <S.NavWrapper>
+            <NavLink to={PATH.ABIBAS}>Abibas</NavLink>
+          </S.NavWrapper>
+          <S.NavWrapper>
+            <NavLink to={PATH.PRICES}>Prices</NavLink>
+          </S.NavWrapper>
+          <S.NavWrapper>
+            <NavLink to={PATH.PROTECTED}>Protected</NavLink>
+          </S.NavWrapper>
+        </div>
+        <div className={styles.content}>
+          <Outlet />
+        </div>
+      </div>
+      <div className={styles.footer}>abibas 2023</div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { router } from "./app/routes/router";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
